fd708e30acc768d53b8b38736fe4da7f03a3b216
Rufus Immanuel Raj P <68699122+rufus582@users.noreply.github.com>
2022-06-18 23:38:23 +0530

    Move ui.xml to device tree

--- build.sh
+++ build.sh
@@ -29,16 +29,16 @@ echo ""
 
 echo "Syncing TWRP-11"
 mkdir ~/twrp-11
-cd /drone/src/ && ls
 cd ~/twrp-11
 repo init https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 --depth=1
 repo sync -j 20
-cp /drone/src/ui.xml ~/twrp-11/bootable/recovery/gui/theme/portrait_hdpi/ui.xml || exit 1
 echo ""
 
 echo "Cloning trees"
 cd ~/twrp-11
 git clone https://github.com/rufus582/android_device_recovery_samsung_m31s ~/twrp-11/device/samsung/m31s
+#Copy ui.xml from device tree
+cp ~/twrp-11/device/samsung/m31s/ui.xml ~/twrp-11/bootable/recovery/gui/theme/portrait_hdpi/
 echo ""
 
 echo "Starting Build"
