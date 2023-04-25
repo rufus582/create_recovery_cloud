4e16304b6ebf971d6f2249c9ea60d53864094418
rufus582 <rufus5802@gmail.com>
2023-04-25 11:02:11 +0530

    build.sh: remove code to upload TWRP.img

--- build.sh
+++ build.sh
@@ -7,12 +7,3 @@ export ALLOW_MISSING_DEPENDENCIES=true
 lunch twrp_m31s-eng
 make recoveryimage -j$( nproc --all )
 echo ""
-
-echo "Uploading recovery image"
-cd ~/twrp-11/out/target/product/*
-version=$(cat ~/twrp-11/bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-version=$(echo $version | cut -c1-5)
-
-mv recovery.img TWRP-11-${version}-m31s-$(TZ='Asia/Karachi' date "+%Y%m%d-%H%M").img
-curl -sL https://git.io/file-transfer | sh
-./transfer wet $(ls TWRP*.img)
